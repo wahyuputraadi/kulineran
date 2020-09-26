3e54e2db2271145ae9cc8985ee2b80f8c92f1ee5
wahyuputraadi <wahyuadii516@gmail.com>
2020-09-26 14:22:47 +0700

    update banner

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="container">
-    <div class="row">
+    <div class="row mt-5">
       <div class="col">
-        <h1><strong>Delicius Food Menu,</strong> in Your Gadget</h1>
+        <div class="d-flex h-100">
+          <div class="justify-content-center align-self-center">
+            <h1>
+              <strong>Delicius Food Menu,</strong> <br />
+              in Your Gadget
+            </h1>
+            <p>Ayo segera pilih dan pesan makanan favorit anda</p>
+            <router-link class="btn btn-success" to="/pesan">Pesan  <b-icon-arrow-right></b-icon-arrow-right></router-link>
+          </div>
+        </div>
       </div>
-      <div class="col"></div>
+      <div class="col">
+        <img src="/assets/img/hero-foods.png" width="100%" />
+      </div>
     </div>
   </div>
 </template>
